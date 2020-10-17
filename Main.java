b9de0f42013d46b1880e93f0338ee09a1497a27f
Marcel Ouellette <marcel.ouellette@patelhs.org>
2020-10-17 03:35:25 +0000

    .

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 27;
+
+    System.out.println("I am " + myAge + " years old.");
+    
   }
 }
